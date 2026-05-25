1353a3cb8edb5525e92ba227618d91e0cc57a4ee
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-05-25 12:07:44 +0800

    CONNECTION

--- src/main/java/com/irenejoy15/springboot/todos/entity/User.java
+++ src/main/java/com/irenejoy15/springboot/todos/entity/User.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -46,9 +48,11 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
+    @CreationTimestamp
     @Column(updatable = false, name = "created_at")
     private Date createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
 
