2fae74a5a209677c792e941a4edbcef75360354f
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-05-29 16:06:16 +0800

    CUSTOM FUNCTION IN USER REPOSITORY

--- src/main/java/com/irenejoy15/springboot/todos/repository/UserRepository.java
+++ src/main/java/com/irenejoy15/springboot/todos/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.irenejoy15.springboot.todos.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,7 @@ import com.irenejoy15.springboot.todos.entity.User;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    // Custom Function 
+    @Query("SELECT COUNT(u) FROM User u JOIN u.authorities a WHERE a.authority = 'ROLE_ADMIN'")
+    long countAdminUsers();
 }
--- src/main/java/com/irenejoy15/springboot/todos/service/UserServiceImpl.java
+++ src/main/java/com/irenejoy15/springboot/todos/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.irenejoy15.springboot.todos.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +12,7 @@ import com.irenejoy15.springboot.todos.repository.UserRepository;
 import com.irenejoy15.springboot.todos.response.UserResponse;
 
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -34,5 +36,31 @@ public class UserServiceImpl implements UserService {
             user.getEmail(),
             user.getAuthorities().stream().map(auth->(Authority) auth).toList()
         );
+    }
+
+    @Override
+    public void deleteUser() {
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication(); 
+        if(authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            throw new AccessDeniedException("User is not authenticated | Access Denied");
+        }
+        User user = (User) authentication.getPrincipal();
+
+        if(isLastAdmin(user)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Cannot delete the last admin user");
+        }
+        userRepository.delete(user);
+    }
+
+    private boolean isLastAdmin(User user) {
+       boolean isAdmin = user.getAuthorities().stream()
+            .anyMatch(authority -> "ROLE_ADMIN".equals(authority.getAuthority()));
+
+        if(isAdmin) {
+            // LAST ADMIN CHECK
+            long adminCount = userRepository.countAdminUsers();
+            return adminCount <= 1; // If there's only one admin, return true
+        }
+        return false;
     }
 }
